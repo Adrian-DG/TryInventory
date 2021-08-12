208adaefb45373b646bc702a075b643c34e8ac3f
Adrian-DG <adrian96guilamo@gmail.com>
2021-08-12 14:06:40 -0400

    navbar and Home components added

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
