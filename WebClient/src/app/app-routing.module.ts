485527dc55f50df4261e021406d10fad1117a08f
Adrian-DG <adrian96guilamo@gmail.com>
2021-08-12 22:37:49 -0400

    register-form and authService added

--- WebClient/src/app/app-routing.module.ts
+++ WebClient/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './Pages/home/home.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+
+const routes: Routes = [
+	{ path: 'dashboard/{id}', component: DashboardComponent },
+	{ path: 'home', component: HomeComponent },
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -4,21 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './components/navbar/navbar.component';
+
+/* ----- modules ---------- */
+
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+/* --- components ------*/
+
+import { NavbarComponent } from './Components/navbar/navbar.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { RegisterFormComponent } from './Components/register-form/register-form.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		HomeComponent,
+		RegisterFormComponent,
+		DashboardComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		NgbModule,
+		HttpClientModule,
+		ReactiveFormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
