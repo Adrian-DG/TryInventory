24bcc77b1fcb8de764cd870a31bd48949db94e01
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2021-09-07 11:59:25 -0400

    changes to controllers, implement GenericController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,86 +8,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class ProductsController : BaseController
+    public class ProductsController : GenericController<Product>
     {
-        private IUnitOfWork<Product> _uof;
-        private IGenericRepository<Product> _repo;
-
-        public ProductsController(IUnitOfWork<Product> unitOfWork)
+        public ProductsController(IUnitOfWork<Product> unitOfWork) : base(unitOfWork)
         {
-            _uof = unitOfWork;
-            _repo = _uof.Repository;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult> GetAll([FromQuery] QueryParams parameters)
-        {
-            try 
-            {
-                return  new JsonResult(await _repo.GetAll(parameters));
-            }
-            catch(Exception ex)
-            {
-                return new JsonResult(ex.Message);
-            }
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult> GetById(int id)
-        {
-            try 
-            {
-                return new JsonResult(await _repo.GetById(id));
-            }
-            catch(Exception ex)
-            {
-                return new JsonResult(ex.Message);
-            }
-        }
-
-        [Authorize(Roles = Roles.Admin)]
-        [HttpPost("create")]
-        public async Task<ActionResult> Create(Product model)
-        {
-            try 
-            {
-                await _repo.Insert(model);
-                return  new JsonResult(await _uof.CommitChanges()); 
-            }
-            catch(Exception ex)
-            {
-                return new JsonResult(ex.Message);
-            }
-        }
-
-        [Authorize(Roles = Roles.Admin)]
-        [HttpPut("edit/{id}")]
-        public async Task<ActionResult> Update(Product model)
-        {
-            try 
-            {
-                _repo.Update(model);
-                return new JsonResult(await _uof.CommitChanges());
-            }
-            catch(Exception ex)
-            {
-                return new JsonResult(ex.Message);
-            }
-        }
-
-        [Authorize(Roles = Roles.Admin)]
-        [HttpDelete("remove/{id}")]
-        public async Task<ActionResult> Delete(int id)
-        {
-             try 
-            {
-                await _repo.Delete(id);
-                return new JsonResult(await _uof.CommitChanges());
-            }
-            catch(Exception ex)
-            {
-                return new JsonResult(ex.Message);
-            }
         }
     }
 }
