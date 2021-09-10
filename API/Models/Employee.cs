dd1ef0b9d64110f75dd3629c4b814045607bb3f4
Adrian-DG <adrian96guilamo@gmail.com>
2021-09-10 00:07:22 -0400

    Add primary key to employee

--- API/Models/Employee.cs
+++ API/Models/Employee.cs
@@ -1,10 +1,14 @@
 using API.Models;
 using API.Abstraction;
+using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace API.Models
 {
     public class Employee : IUserData 
-    {
+    {   
+        [Key]
+        public Guid Id { get; set; }
         public AppUser User { get; set; }
     }
 }
